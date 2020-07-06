33c7a20772858ffc4f036a6c14c69f091a9305f0
Rene Skov <Skov@simpelapps.dk>
2020-07-06 23:16:46 +0200

    Demo
    added support for UItextfield

--- AnimatedText_Example/AnimatedText_Example/ViewController.swift
+++ AnimatedText_Example/AnimatedText_Example/ViewController.swift
@@ -2,9 +2,27 @@
 //  ViewController.swift
 //  AnimatedText_Example
 //
-//  Created by Rene Skov on 02/07/2020.
-//  Copyright © 2020 Rene Skov. All rights reserved.
+// The MIT License (MIT)
 //
+//  Created by Rene Skov on 02/07/2020.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
 import AnimatedText
@@ -35,6 +53,7 @@ class ViewController: UIViewController {
             })
        }
     
+    // MARK: animate textView typing........
    @IBAction func startTyping(_ button:UIButton){
         // typing without any wrapping
         let message = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
